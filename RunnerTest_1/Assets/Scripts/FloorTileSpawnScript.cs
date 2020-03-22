7feb08372e9f3e94c24ddb9d425a69b3dd66b954
zeta1reticuli <space1signs@gmail.com>
2020-03-22 23:39:57 +0300

    Added floor tiles pool, no spawning yet

--- RunnerTest_1/Assets/Scripts/FloorTileSpawnScript.cs
+++ RunnerTest_1/Assets/Scripts/FloorTileSpawnScript.cs
@@ -4,46 +4,30 @@ using UnityEngine;
 
 public class FloorTileSpawnScript : MonoBehaviour
 {
-    public GameObject[] floorTile;
-   
-    private float tileLength = 10;
-    private GameObject player;
-    public float amountOfTilesOnScreen = 10;
-    private float spawnStart = -10;
-    
+    public GameObject floorTile;
+    public List<GameObject> floorTilesPool;
+    public int floorTilesPoolAmount = 10;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        for (int i = 0; i < amountOfTilesOnScreen; i++)
+        floorTilesPool = new List<GameObject>();
+        for (int i = 0; i < floorTilesPoolAmount; i++)
         {
-            SpawnTile();
+            GameObject obj = (GameObject)Instantiate(floorTile); //creates placeholder object and instantiates needed object to it
+            obj.SetActive(false); //object is deactivated for now
+            obj.transform.SetParent(transform); //sets parent to this (empty spawner) object
+            floorTilesPool.Add(obj); //adds obj to the list
         }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //if (player.transform.z )
-        //SpawnTile();
-        //DeleteTile();
+        
     }
-    void SpawnTile()
-    {
-        GameObject tile;
-        tile = Instantiate(floorTile[0]) as GameObject;
-        tile.transform.SetParent(transform);
-        tile.transform.position = Vector3.forward * spawnStart; //spawns tiles at z
-        spawnStart += tileLength; //increases z by 10 
+   
 
-
-    }
-
-    void DeleteTile()
-    {
-
-    }
+  
 }
