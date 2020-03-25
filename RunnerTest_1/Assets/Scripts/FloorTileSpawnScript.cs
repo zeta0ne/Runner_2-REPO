1d2509efb21a5cc4281f48b4d968e25b670870d7
zeta1reticuli <space1signs@gmail.com>
2020-03-25 06:03:03 +0300

    Spawns 10 tiles in the beginning

--- RunnerTest_1/Assets/Scripts/FloorTileSpawnScript.cs
+++ RunnerTest_1/Assets/Scripts/FloorTileSpawnScript.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class FloorTileSpawnScript : MonoBehaviour
 {
+    //[SerializeField]
     public GameObject floorTile;
+
     public List<GameObject> floorTilesPool;
-    public int floorTilesPoolAmount = 10;
-    
+    public int floorTilesPoolAmount = 20;
+    public int floorTilesOnScreen = 10;
+    private float floorTileLength = 10f;
+ 
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         floorTilesPool = new List<GameObject>();
         for (int i = 0; i < floorTilesPoolAmount; i++)
         {
-            GameObject obj = (GameObject)Instantiate(floorTile); //creates placeholder object and instantiates needed object to it
-            obj.SetActive(false); //object is deactivated for now
-            obj.transform.SetParent(transform); //sets parent to this (empty spawner) object
-            floorTilesPool.Add(obj); //adds obj to the list
+            GameObject myFloorTile = Instantiate(floorTile) as GameObject; //creates placeholder object and instantiates prefab into it
+            
+            myFloorTile.SetActive(false); //object is deactivated for now
+            myFloorTile.transform.SetParent(transform); //sets parent to this (empty spawner) object
+            floorTilesPool.Add(myFloorTile); //adds obj to the list
         }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < floorTilesPool.Count; i++)
+        {
+            if (!floorTilesPool[i].activeInHierarchy)
+            {
+                return floorTilesPool[i];
+            }
+        }
+        return null;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        GetPooledObject();
+        if (floorTile != null)
+        {
+            for (int i = 0; i < floorTilesOnScreen; i++)
+            {
+                floorTilesPool[i].SetActive(true);
+                floorTilesPool[i].transform.position = new Vector3(transform.position.x, transform.position.y, (transform.position.z + floorTileLength) * i);
+            }
+        }  
+    }
+
+ 
+
+    void DeactivateTile()
+    {
+        floorTile.SetActive(false);
     }
    
 
